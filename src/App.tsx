43b035b8744a966ab5b467701e182d892624c188
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-11-02 13:40:01 +0100

    Files added to file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import Home from "./_root/pages/Home";
 
 const App = () => {
-  return <button className="btn">Hello world!</button>;
+  return (
+    <>
+      <main>
+        <Routes>
+          {/* Public routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-in" element={<SignupForm />} />
+          </Route>
+
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* Private routes */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
