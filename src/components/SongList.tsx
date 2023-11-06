baeef0e9629bd906458ae050828deaf3a7c85773
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-11-06 22:41:43 +0100

    First setup modal and song list

---
+++ src/components/SongList.tsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import { database, storage } from "@/lib/appwrite/config";
+
+const SongList = () => {
+  const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const databaseId = import.meta.env.VITE_APPWRITE_SONGS_DATABASE_ID;
+  const collectionId = import.meta.env.VITE_APPWRITE_SONGS_COLLECTION_ID;
+  const audioBucketId = import.meta.env.VITE_APPWRITE_AUDIOFILES_BUCKET_ID;
+  const imageBucketId = import.meta.env.VITE_APPWRITE_IMAGES_BUCKET_ID;
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const fetchSongs = async () => {
+    try {
+      const result = await database.listDocuments(databaseId, collectionId);
+      setSongs(result.documents);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteSong = async (songId) => {
+    // alert om te vragen of de gebruiker zeker is
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this song?"
+    );
+    if (!confirmed) return;
+
+    try {
+      await database.deleteDocument(databaseId, collectionId, songId);
+      setSongs(songs.filter((song) => song.$id !== songId));
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const getFileURL = (bucketId, fileId) => {
+    return storage.getFilePreview(bucketId, fileId);
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  const getDownloadableFileURL = (bucketId, fileId) => {
+    return storage.getFileDownload(bucketId, fileId);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center dark:text-white text-black text-4xl font-bold font-main">
+        Music Manager
+      </h1>
+      <ul className="w-full">
+        {songs.map((song) => (
+          <li
+            key={song.$id}
+            className="flex items-center justify-between gap-10 "
+          >
+            <img
+              src={getDownloadableFileURL(imageBucketId, song["image-id"])}
+              alt={song.title}
+              className="w-20"
+            />
+            <div>
+              <h3>{song.title}</h3>
+
+              <audio controls>
+                <source
+                  src={getDownloadableFileURL(
+                    audioBucketId,
+                    song["audiofile-id"]
+                  )}
+                  type="audio/mpeg"
+                />
+                Your browser does not support the audio element.
+              </audio>
+            </div>
+            <button
+              onClick={() => deleteSong(song.$id)}
+              className="btn btn-secondary "
+            >
+              Verwijder
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
---
+++ src/components/modals/AddSong.tsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { database, storage } from "@/lib/appwrite/config";
+function AddSong({ modalOpen, closeModal }) {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const databaseId = import.meta.env.VITE_APPWRITE_SONGS_DATABASE_ID;
+  const collectionId = import.meta.env.VITE_APPWRITE_SONGS_COLLECTION_ID;
+  const audioBucketId = import.meta.env.VITE_APPWRITE_AUDIOFILES_BUCKET_ID;
+  const imageBucketId = import.meta.env.VITE_APPWRITE_IMAGES_BUCKET_ID;
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleAudioChange = (e) => {
+    setAudio(e.target.files[0]);
+  };
+
+  // Functie om een afbeeldingsbestand te uploaden
+  async function uploadImage(file) {
+    try {
+      const result = await storage.createFile(imageBucketId, "unique()", file);
+      return result.$id; // De ID van het geüploade bestand
+    } catch (error) {
+      console.error("Fout bij het uploaden van de afbeelding:", error);
+      throw error; // Gooi de fout zodat je deze kunt afhandelen
+    }
+  }
+
+  // Functie om een audiobestand te uploaden
+  async function uploadAudio(file) {
+    try {
+      const result = await storage.createFile(audioBucketId, "unique()", file);
+      return result.$id; // De ID van het geüploade bestand
+    } catch (error) {
+      console.error("Fout bij het uploaden van het audiobestand:", error);
+      throw error; // Gooi de fout zodat je deze kunt afhandelen
+    }
+  }
+
+  // Aangepaste addSong functie
+  async function addSong(title, imageFile, audioFile) {
+    setLoading(true); // Zet laden aan het begin van de functie
+    try {
+      // Upload de afbeelding en het audiobestand en krijg hun ID's
+      const imageId = await uploadImage(imageFile);
+      const audiofileId = await uploadAudio(audioFile);
+
+      // Maak nu het document in de collectie met de verkregen ID's
+      const songResult = await database.createDocument(
+        databaseId,
+        collectionId,
+        "unique()", // Dit zorgt voor een unieke ID voor het document
+        {
+          title: title,
+          "image-id": imageId,
+          "audiofile-id": audiofileId,
+        },
+        [], // Lees rechten
+        [] // Schrijf rechten
+      );
+    } catch (error) {
+      console.error(
+        "Er is een fout opgetreden bij het toevoegen van het nummer:",
+        error
+      );
+    } finally {
+      setLoading(false); // Zet laden uit aan het einde van de functie
+    }
+  }
+
+  return (
+    <form className="flex flex-col justify-center items-center gap-4 fixed bg_background dark:bg-background_dark z-10 shadow-lg shadow-primary border-2 rounded-lg p-5">
+      <button onClick={closeModal}>X</button>
+      <h2 className="text-center dark:text-white text-black text-3xl font-bold font-main">
+        Nieuw nummer
+      </h2>
+      <div>
+        <label>
+          Titel: <br />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            className="inputform"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Afbeelding:
+          <br />
+          <input
+            type="file"
+            onChange={handleImageChange}
+            accept="image/*"
+            required
+            className="inputform"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Audio:
+          <br />
+          <input
+            type="file"
+            onChange={handleAudioChange}
+            accept="audio/*"
+            required
+          />
+        </label>
+      </div>
+      <button
+        type="button"
+        className="btn-secondary btn text-lg"
+        disabled={loading}
+        onClick={() => {
+          if (!loading) {
+            addSong(title, image, audio);
+          }
+        }}
+      >
+        {loading ? "Laden..." : "Voeg toe"}
+      </button>
+    </form>
+  );
+}
+
+export default AddSong;
