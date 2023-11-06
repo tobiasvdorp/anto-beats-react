3eadd380eebed22d1c8ae12275ce8f8b6ca45bbf
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-11-06 22:08:13 +0100

    Account created change

--- src/_auth/forms/AccountCreated.tsx
+++ src/_auth/forms/AccountCreated.tsx
@@ -7,9 +7,7 @@ const AccountCreated = () => {
         <h1 className="dark:text-white text-black text-4xl font-bold font-main text-center mb-3">
           Account created succesfully!
         </h1>
-        <p className="dark:text-gray-500 text-black text-center mb-5 text-lg">
-          Verify your email, then sign in.
-        </p>
+
         <Link to="/sign-in" className="btn-secondary btn w-40 text-lg">
           Sign in
         </Link>
