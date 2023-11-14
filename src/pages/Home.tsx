da304dc529235117f95c617b74d98e0421d2d326
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-11-14 19:26:00 +0100

    New layer added to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,15 +10,15 @@ const Home = () => {
         <ParallaxLayer offset={0} speed={0} factor={5} />
 
         {/* Call to action */}
-        <ParallaxLayer offset={0} speed={0.3} factor={0.8}>
+        <ParallaxLayer offset={0} speed={0.3} factor={0.5}>
           <CallToAction />
         </ParallaxLayer>
 
         {/* Waves */}
         <ParallaxLayer
-          offset={0.8}
-          speed={1}
-          factor={1}
+          offset={0.9}
+          speed={1.4}
+          factor={1.3}
           style={{
             backgroundImage: `url(/waves.svg)`,
             backgroundSize: "contain",
@@ -28,11 +28,31 @@ const Home = () => {
 
         {/* Audioplayer */}
         <ParallaxLayer
-          offset={0.99}
-          speed={2}
+          offset={0.9}
+          speed={1}
+          factor={0.8}
           className="flex items-center justify-center mt-60"
         >
           <AudioPlayer isHome={isHome} />
+        </ParallaxLayer>
+
+        {/* Waves */}
+        <ParallaxLayer
+          offset={1.1}
+          speed={1.4}
+          factor={1.3}
+          style={{
+            backgroundImage: `url(/waves2.svg)`,
+            backgroundSize: "contain",
+            backgroundRepeat: "repeat",
+          }}
+          className="flex items-center justify-center "
+        ></ParallaxLayer>
+        {/* DIV */}
+        <ParallaxLayer offset={1.2} factor={0.8} speed={1}>
+          <div className="bg-red-700 mt-96 flex items-center justify-center w-full">
+            ha
+          </div>
         </ParallaxLayer>
       </Parallax>
     </div>
