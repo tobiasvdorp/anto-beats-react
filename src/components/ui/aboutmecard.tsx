c514c35006aa5edf4c7bc9917e90f36ada407032
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-11-22 21:37:31 +0100

    Error bug fix

--- src/components/ui/aboutmecard.tsx
+++ src/components/ui/aboutmecard.tsx
@@ -15,26 +15,29 @@ const AboutMeCard = ({ cardId, title, paragraph, position }) => {
   const [formActive, setFormActive] = useState(false);
   const [value, setValue] = useState(paragraph);
   const [loading, setLoading] = useState(false);
-  const [cardTitle, setCardTitle] = useState(title); // Nu is het een string
+  const [cardTitle, setCardTitle] = useState(title);
   const [cardParagraph, setCardParagraph] = useState(paragraph);
-  const [error, setError] = useState(""); // Error state voor het formulier
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
+    if (cardTitle === title && cardParagraph === paragraph) {
+      setFormActive(false);
+      setLoading(false);
+      return;
+    }
     try {
       const updatedCard = await updateAboutMeCard(cardId, cardTitle, value);
 
-      // Bijwerken van de lokale state na een succesvolle update
-      setCardParagraph(updatedCard.paragraph); // of value
-      setCardTitle(updatedCard.title); // of cardTitle
-      setFormActive(false); // Sluit het formulier alleen als de update succesvol is
-      setError(""); // Wis foutmeldingen
+      setCardParagraph(updatedCard.paragraph);
+      setCardTitle(updatedCard.title);
+      setFormActive(false);
+      setError("");
     } catch (error) {
       console.error("Failed to update card:", error);
       setError("Error updating card. Please try again later.");
-      // Hier niet sluiten van het formulier en niet bijwerken van de lokale state
     } finally {
       setLoading(false);
     }
@@ -47,54 +50,38 @@ const AboutMeCard = ({ cardId, title, paragraph, position }) => {
       highlight={false}
     >
       <div
-        className={`relative border-background_dark border-4 m-7 bg-primary_dark p-4 text-black animate__animated max-w-2xl rounded-xl depth-transform 
+        className={`flex flex-col gap-2 relative border-background_dark border-4 my-10 bg-primary_dark p-4 text-black animate__animated max-w-2xl rounded-xl depth-transform 
     ${position === "left" ? "depth-transform" : "depth-transform-2"}
     `}
         data-atropos-offset="0"
       >
-        {/* Title that can be changed */}
-        <h2
-          data-atropos-offset="1"
-          className={`font-main font-vtc tracking-widest capitalize text-2xl absolute -top-5 left-5   bg-primary_dark p-2 rounded-xl ${
-            formActive ? "hidden" : ""
-          }`}
-        >
-          {cardTitle}
-          {isAdmin && (
-            <button className="pl-2" onClick={() => setFormActive(true)}>
-              <FaEdit className="hover:text-accent duration-200" />
-            </button>
-          )}
-        </h2>
-        {/* Form for changing the title */}
-        {formActive && (
-          <form className="w-full">
-            <input
-              defaultValue={cardTitle}
-              className="font-main bg-primary_dark p-1 border-2 border-primary w-full rounded-md"
-              onChange={(e) => setCardTitle(e.target.value)}
-            />
-            <br />
-            <div className="flex items-center gap-1">
-              <button
-                className="btn-secondary btn"
-                onClick={() => setFormActive(false)}
-              >
-                Cancel
+        <div>
+          {/* Title that can be changed */}
+          <h2
+            data-atropos-offset="1"
+            className={`font-main font-vtc tracking-widest capitalize text-2xl absolute -top-8 left-5   bg-primary_dark p-2 rounded-xl ${
+              formActive ? "hidden" : ""
+            }`}
+          >
+            {cardTitle}
+            {isAdmin && (
+              <button className="pl-2" onClick={() => setFormActive(true)}>
+                <FaEdit className="hover:text-accent duration-200" />
               </button>
-              {loading ? (
-                <button className="btn btn-secondary" disabled>
-                  <span className="loading loading-spinner loading-sm"></span>{" "}
-                  Loading...
-                </button>
-              ) : (
-                <button className="btn btn-secondary" onClick={handleSubmit}>
-                  Save
-                </button>
-              )}
-            </div>
-          </form>
-        )}
+            )}
+          </h2>
+          {/* Form for changing the title */}
+          {formActive && (
+            <form className="w-full">
+              <input
+                defaultValue={cardTitle}
+                className="font-main bg-primary_dark p-1 border-2 border-primary w-full rounded-md"
+                onChange={(e) => setCardTitle(e.target.value)}
+              />
+              <br />
+            </form>
+          )}
+        </div>
         {/* Paragraph that can be changed */}
         <p
           data-atropos-offset="2"
@@ -117,18 +104,24 @@ const AboutMeCard = ({ cardId, title, paragraph, position }) => {
             <br />
             <div className="flex items-center gap-1">
               <button
-                className="btn-secondary btn"
+                className="btn-secondary btn px-2 min-h-0 h-10 first-btn"
                 onClick={() => setFormActive(false)}
               >
                 Cancel
               </button>
               {loading ? (
-                <button className="btn btn-secondary" disabled>
+                <button
+                  className="btn btn-secondary px-2 min-h-0 h-10"
+                  disabled
+                >
                   <span className="loading loading-spinner loading-sm"></span>{" "}
                   Loading...
                 </button>
               ) : (
-                <button className="btn btn-secondary" onClick={handleSubmit}>
+                <button
+                  className="btn btn-secondary px-10 min-h-0 h-10 border-none"
+                  onClick={handleSubmit}
+                >
                   Save
                 </button>
               )}
